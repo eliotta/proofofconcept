c99c4598232f091fbc52a58fe20b01a6c18ebe33
unknown <eliotta@outlook.com>
2020-01-13 09:35:19 -0500

    four cxs1w

---
+++ Start4.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+
+@SuppressWarnings("Duplicates")
+public class Start4
+{
+    public static void main(String[] args)
+    {
+        start4();
+    }
+
+    public static void start4()
+    {
+        System.out.println(" in start4  "); 
+    }
+
+}
+
+
